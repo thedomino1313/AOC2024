d8791b3e371fd7fb5169e735d62f1f961c7e41d9
Dominic Beyer <90935455+thedomino1313@users.noreply.github.com>
2024-12-04 10:42:42 -0500

    Update 12-04-2024.py
    "why are you even doing it in a with, just use the same text as from part 1'

--- 12-04-2024.py
+++ 12-04-2024.py
@@ -1,5 +1,8 @@
 from utils import *
 
+with open(FILENAME, 'r') as f:
+    text = list(map(lambda x: list(x.strip()), f.readlines()))
+
 ## part 1
 def search(text, posx, posy, dir_=(), letter = 0):
     string = "XMAS"
@@ -11,13 +14,10 @@ def search(text, posx, posy, dir_=(), letter = 0):
             return search(text, posx+dir_[0], posy+dir_[1], dir_, letter+1)
     return 0
 
-with open(FILENAME, 'r') as f:
-    text = list(map(lambda x: list(x.strip()), f.readlines()))
-    print(sum(search(text, i, j) for i in range(len(text)) for j in range(len(text[0])) if text[i][j] == "X"))
+print(sum(search(text, i, j) for i in range(len(text)) for j in range(len(text[0])) if text[i][j] == "X"))
 
 ## part 2
 def search(text, posx, posy):
     return set((text[posx-1][posy-1], text[posx+1][posy+1])) == set(("M","S")) == set((text[posx+1][posy-1], text[posx-1][posy+1]))
 
-with open(FILENAME, 'r') as f:
-    print(sum(search(text, i, j) for i in range(1, len(text[1:])) for j in range(1, len(text[0][1:])) if text[i][j] == "A"))
+print(sum(search(text, i, j) for i in range(1, len(text[1:])) for j in range(1, len(text[0][1:])) if text[i][j] == "A"))
