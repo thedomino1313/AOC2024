f53484bd3b8f589e08219b9e4294a1d7335448d7
Dominic Beyer <90935455+thedomino1313@users.noreply.github.com>
2024-12-04 11:01:31 -0500

    Update 12-04-2024.py
    minor improvements

--- 12-04-2024.py
+++ 12-04-2024.py
@@ -9,7 +9,7 @@ def search(text, posx, posy, dir_=(), letter = 0):
     if letter == 4: return 1
     if 0 > posx or posx >= len(text) or 0 > posy or posy >= len(text[0]): return 0
     if not dir_:
-        return sum([search(text, posx+i, posy+j, (i,j), 1) for i in [-1, 0, 1] for j in [-1, 0, 1] if i or j])
+        return sum(search(text, posx+i, posy+j, (i,j), 1) for i,j in product([-1, 0, 1], repeat=2) if i or j)
     if text[posx][posy] == string[letter]:
             return search(text, posx+dir_[0], posy+dir_[1], dir_, letter+1)
     return 0
@@ -18,6 +18,6 @@ print(sum(search(text, i, j) for i in range(len(text)) for j in range(len(text[0
 
 ## part 2
 def search(text, posx, posy):
-    return set((text[posx-1][posy-1], text[posx+1][posy+1])) == set(("M","S")) == set((text[posx+1][posy-1], text[posx-1][posy+1]))
+    return set((text[posx-1][posy-1], text[posx+1][posy+1])) == set("MS") == set((text[posx+1][posy-1], text[posx-1][posy+1]))
 
 print(sum(search(text, i, j) for i in range(1, len(text[1:])) for j in range(1, len(text[0][1:])) if text[i][j] == "A"))
